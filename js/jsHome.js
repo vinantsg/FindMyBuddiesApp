0ccc31460e2497971d9c40afb36574c2ce576fcd
vinantsg <vinant.singh24@gmail.com>
2014-06-29 03:26:42 +0530

    POC
    Done geolocation

---
+++ js/jsHome.js
@@ -0,0 +1,29 @@
+document.addEventListener("deviceready", onDeviceReady, true);
+function onDeviceReady() {
+    $('#btnGetLocation').click(function(){
+        alert('going to call geo location');
+         navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    });
+      
+}
+// onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        alert('success');
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          +           position.timestamp          + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
